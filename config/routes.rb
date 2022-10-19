b6cd5e2618e3e76123f138882b2dcf19b05818db
Michael Chong <mikec0920@gmail.com>
2022-10-19 12:20:19 -0700

    Generate kittens controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'kittens/index'
+  get 'kittens/show'
+  get 'kittens/new'
+  get 'kittens/create'
+  get 'kittens/edit'
+  get 'kittens/update'
+  get 'kittens/delete'
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "kittens#index"
 end
