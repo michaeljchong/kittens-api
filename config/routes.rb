8adfef9f3b7fb8a10eb95d86fbe85f65005e71bd
Michael Chong <mikec0920@gmail.com>
2022-10-19 17:10:34 -0700

    Resourceful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'kittens/index'
-  get 'kittens/show'
-  get 'kittens/new'
-  get 'kittens/create'
-  get 'kittens/edit'
-  get 'kittens/update'
-  get 'kittens/delete'
+  resources :kittens
 
   # Defines the root path route ("/")
   root "kittens#index"
