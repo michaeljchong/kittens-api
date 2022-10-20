9c11f171152df0f68bd5ebe1e7c3731a27e53a5f
Michael Chong <mikec0920@gmail.com>
2022-10-20 12:26:54 -0700

    Add flash status messages

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -15,10 +15,10 @@ class KittensController < ApplicationController
     @kitten = Kitten.new(kitten_params)
 
     if @kitten.save
-      flash[:success] = "Meow! There's one more kitten!"
+      flash.now[:success] = "Meow! There's one more kitten!"
       redirect_to @kitten
     else
-      flash.now[:error] = "Kitten details submitted incorrectly"
+      flash.now[:error] = "Bark! Kitten details submitted incorrectly"
       render :new, status: :unprocessable_entity
     end
   end
@@ -31,18 +31,20 @@ class KittensController < ApplicationController
     @kitten = Kitten.find(params[:id])
 
     if @kitten.update(kitten_params)
+      flash.now[:success] = "Meow! You edited a kitten!"
       redirect_to @kitten
     else
-      flash.now[:error] = "Kitten details submitted incorrectly"
+      flash.now[:error] = "Bark! Kitten details submitted incorrectly"
       render :new, status: :unprocessable_entity
     end
   end
 
   def delete
     @kitten = Kitten.find(params[:id])
-    @kitten.destroy
-
-    redirect_to root_path, status: :see_other
+    if @kitten.destroy
+      flash.now[:success] = "You released a kitten into the wild!"
+      redirect_to root_path, status: :see_other
+    end
   end
 
   private
