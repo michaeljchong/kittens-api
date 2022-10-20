3625570bf683022195df4b164615fe771af4c165
Michael Chong <mikec0920@gmail.com>
2022-10-19 17:12:17 -0700

    Write new and create methods

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -8,9 +8,19 @@ class KittensController < ApplicationController
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      flash[:success] = "Meow! There's one more kitten!"
+      redirect_to @kitten
+    else
+      flash.now[:error] = "Kitten details submitted incorrectly"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
